fd17b9dfd58fb5738852a26277916a2b77436a8f
mcgourtyalex <mcgourty.alex@berkeley.edu>
2014-07-11 14:05:07 -0700

    extrapolated new functionality

--- custom_widgets.php
+++ custom_widgets.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once( PATH . '\populate_list.php');
 require_once( PATH . '\clientID.php');
 
 // Populate the widget:
@@ -11,9 +10,7 @@ function pages_widget_content() {
 
         // Insert the iFrame with analytics.html inside
         echo '<iframe id="targetFrame" height="550" src="../wp-content/plugins/google-analytics-page-list/analytics.html" width="100%"></iframe>';
-    
-        // Populate the Page and Post List
-        populateList();
+
     }
 }
 ?>
--- tdClick.js
+++ tdClick.js
@@ -1,8 +1,22 @@
 $(document).ready(function ()
 {
-    $('#sele').click(function ()
+    $('#weekDiv').click(function ()
     {
-        var $obj = $('.gapi-analytics-data-chart-table-styles-tr-selected').first();
+        var $obj = $('#weekDiv .gapi-analytics-data-chart-table-styles-tr-selected').first();
+        console.log($obj.contents().html());
+        $('#pageSelector').val($obj.contents().html());
+    });
+
+    $('#monthDiv').click(function ()
+    {
+        var $obj = $('#monthDiv .gapi-analytics-data-chart-table-styles-tr-selected').first();
+        console.log($obj.contents().html());
+        $('#pageSelector').val($obj.contents().html());
+    });
+
+    $('#yearDiv').click(function ()
+    {
+        var $obj = $('#yearDiv .gapi-analytics-data-chart-table-styles-tr-selected').first();
         console.log($obj.contents().html());
         $('#pageSelector').val($obj.contents().html());
     });
